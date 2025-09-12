2b056789309b7c129cbeb49779e1a07915c95214
Filipp947 <filipp69420@gmail.com>
2025-09-13 01:50:12 +0300

    Update joke.js

--- api/joke.js
+++ api/joke.js
@@ -1,16 +1,11 @@
 import fs from "fs"
 import path from "path"
 
-let jokes = null
-
 export default function handler(req, res) {
   try {
-    // load once on cold start
-    if (!jokes) {
-      const filePath = path.join(process.cwd(), "reddit_jokes.json")
-      const data = fs.readFileSync(filePath, "utf-8")
-      jokes = JSON.parse(data)
-    }
+    const filePath = path.join(process.cwd(), "reddit_jokes.json")
+    const data = fs.readFileSync(filePath, "utf-8")
+    const jokes = JSON.parse(data)
 
     const randomJoke = jokes[Math.floor(Math.random() * jokes.length)]
     res.status(200).json(randomJoke)
