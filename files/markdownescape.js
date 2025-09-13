1aa22e0f0cca47d93954c4b7af3ffcf69e048b50
Filipp947 <filipp69420@gmail.com>
2025-09-13 12:54:12 +0300

    Create markdownescape.js

---
+++ files/markdownescape.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  const q = req.query.q;
+  if(!q) return res.status(400).json({ error:"q missing" });
+  const result = q.replace(/([*_~`|])/g,'\\$1');
+  res.status(200).json({ result });
+}
